46f7eeff7a9ff057e47d7c289008611586258f49
SizhenChen <chens656@newschool.edu>
2023-04-17 12:41:06 -0400

    up

--- data.php
+++ data.php
@@ -1,4 +1,17 @@
- <?php echo $_POST["fname"]; ?><br>
- <?php echo $_POST["lname"]; ?><br>
- <?php echo $_POST["Email"]; ?><br>
- <?php echo $_POST["subject"]; ?>
+ <?php 
+
+ if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $Email = $_POST['Email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+ 
+    $mailTo = "sizhenchen2020@163.com";
+    $headers = "From: ".$Email;
+    $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: Contact.html?mailsend");
+ }
+
+ ?>
